beb850e4c4ccfabc0169778f962e80890c112dab
APIVerve Team <hello@apiverve.com>
2024-05-14 15:41:51 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,53 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class data
+{
+    [JsonProperty("starName")]
+    public string starName { get; set; }
+
+    [JsonProperty("mass")]
+    public double mass { get; set; }
+
+    [JsonProperty("diameter")]
+    public int diameter { get; set; }
+
+    [JsonProperty("galX")]
+    public double galX { get; set; }
+
+    [JsonProperty("galY")]
+    public double galY { get; set; }
+
+    [JsonProperty("galZ")]
+    public double galZ { get; set; }
+
+    [JsonProperty("dist")]
+    public double dist { get; set; }
+
+    [JsonProperty("starType")]
+    public string starType { get; set; }
+
+    [JsonProperty("temp")]
+    public int temp { get; set; }
+
+    [JsonProperty("color")]
+    public string color { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
